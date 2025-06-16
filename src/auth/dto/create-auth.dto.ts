2e411b86136da1151c41a9055efca09901e2aafd
Vo Xuan Y <voxuany2622004@gmail.com>
2025-06-16 19:24:05 +0700

    fix

--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -15,10 +15,10 @@ import {
     @IsString()
     @IsNotEmpty({ message: 'Password is required.' })
     @MinLength(6, { message: 'Password must be at least 6 characters long.' })
-    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
-      message:
-        'Password must contain at least 1 uppercase letter, 1 lowercase letter, and 1 number or special character.',
-    })
+    // @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    //   message:
+    //     'Password must contain at least 1 uppercase letter, 1 lowercase letter, and 1 number or special character.',
+    // })
     password: string;
   
    
